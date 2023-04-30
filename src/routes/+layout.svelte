2263c23430cc1b4d7dd2cd634474cfd16954a1f8
Forrest Dunlap <forrestmdunlap@gmail.com>
2023-04-30 12:00:28 -0400

    Add Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
 	import HeaderLogo from '../components/HeaderLogo.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="flex min-h-screen w-full flex-col bg-zinc-300">
