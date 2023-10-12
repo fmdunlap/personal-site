0ac83c14b3e412ccd840084f1b709b664d89bb8d
Forrest Dunlap <forrestmdunlap@gmail.com>
2023-10-12 18:26:51 -0400

    Add posts & post slug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
